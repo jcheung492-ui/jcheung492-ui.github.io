0920f1cdf4ac3128e5f451550eecf5dfba25ff94
jcheung492-ui <jcheung492@gmail.com>
2026-06-15 20:43:19 +0800

    内容更新:+1 作品 （管理面板发布）

--- js/published.js
+++ js/published.js
@@ -11,6 +11,17 @@ window.SITE_PUBLISHED = [
     "desc": "时间是多维的平铺 moment是这一刻的散落 ，一个个拾起成了耳边的轰鸣 ，一个个缠绕系结成了面 如果那些时刻都不曾感受和记得 ，那现在的我 ，又是从哪刻才能开始降落",
     "cover": "covers/up-1781450578485-7909.jpg",
     "src": "audio/up-1781450578485-7909.mp3"
+  },
+  {
+    "id": "custom-1781494192844-943",
+    "category": "album",
+    "title": "Dripping",
+    "en": "",
+    "year": "2025",
+    "role": "制作人（Co-Producing）",
+    "desc": "电影放完后看向窗外，下雨了，我们要不要去雨里跳舞？",
+    "cover": "covers/up-1781494192844-943.jpg",
+    "src": "audio/up-1781494192844-943.mp3"
   }
 ];
 window.SITE_HIDDEN = [
