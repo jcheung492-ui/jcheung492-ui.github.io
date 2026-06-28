ad9922c156dae8019a12799d94c6177178ce7fc1
jcheung492-ui <jcheung492@gmail.com>
2026-06-28 12:22:51 +0800

    内容更新:（管理面板发布）

--- js/published.js
+++ js/published.js
@@ -209,16 +209,6 @@ window.SITE_GALLERY_PUBLISHED = [
     "id": "gcustom-1782619119631-964",
     "caption": "电影《寻她》首部长篇音乐",
     "src": "gallery/gup-1782619119631-964.jpg"
-  },
-  {
-    "id": "gcustom-1782620345225-7441",
-    "caption": "电影《寻她》庆功宴with白客",
-    "src": "gallery/gup-1782620345225-7441.jpg"
-  },
-  {
-    "id": "gcustom-1782620483014-9828",
-    "caption": "电影《寻她》庆功宴with舒淇",
-    "src": "gallery/gup-1782620483014-9828.jpg"
   }
 ];
 window.SITE_GALLERY_HIDDEN = [
